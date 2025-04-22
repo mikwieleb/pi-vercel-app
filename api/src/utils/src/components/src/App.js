1dc165d73eef73a0de0c6fc0c2031b30a3db73e7
mikwieleb <mik.wieleb@gmail.com>
2025-04-22 21:51:17 +0200

    Create App.js

---
+++ api/src/utils/src/components/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PiPaymentButton from './components/PiPaymentButton';
+
+const App = () => {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '100px' }}>
+      <h1>Test Paiement Pi Network</h1>
+      <PiPaymentButton />
+    </div>
+  );
+};
+
+export default App;
